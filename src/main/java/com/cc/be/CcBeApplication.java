6467efc602b5cebfde9077e798fee3a0efda7d88
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-11-02 14:52:54 -0500

    first commit

---
+++ src/main/java/com/cc/be/CcBeApplication.java
@@ -0,0 +1,13 @@
+package com.cc.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcBeApplication.class, args);
+	}
+
+}
