17b6c9ace6101b35a7117423c22a1e0123dfc55f
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-11-09 23:47:50 -0500

    feat: added new atribute for Evaluador and Evaluando

---
+++ src/main/java/com/cc/be/controller/LineaInvestigacionController.java
@@ -0,0 +1,41 @@
+package com.cc.be.controller;
+
+import com.cc.be.dto.LineaInvestigacionDTO;
+import com.cc.be.model.LineaInvestigacion;
+import com.cc.be.service.LineaInvestigacionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LineaInvestigacionController {
+    private final LineaInvestigacionService lineaInvestigacionService;
+
+    @PostMapping
+    public LineaInvestigacion create(@RequestBody LineaInvestigacionDTO dto) {
+        return lineaInvestigacionService.createLineaInvestigacion(dto);
+    }
+
+    @GetMapping("/list")
+    public List<LineaInvestigacion> getAll() {
+        return lineaInvestigacionService.getAllLineasInvestigacion();
+    }
+
+    @GetMapping("/{id}")
+    public LineaInvestigacion getById(@PathVariable Long id) {
+        return lineaInvestigacionService.getLineaInvestigacionById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public LineaInvestigacion update(@PathVariable Long id, @RequestBody LineaInvestigacionDTO dto) {
+        return lineaInvestigacionService.updateLineaInvestigacion(id, dto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        lineaInvestigacionService.deleteLineaInvestigacion(id);
+    }
+}
